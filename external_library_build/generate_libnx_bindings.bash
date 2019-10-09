e3c7d6e4a85a68549f5879ea51efa783c8bf0063
uyjulian <uyjulian@gmail.com>
2019-10-08 22:35:40 -0500

    Rename _Bool to bool (for compatibility with SWIG)

--- external_library_build/generate_libnx_bindings.bash
+++ external_library_build/generate_libnx_bindings.bash
@@ -14,16 +14,17 @@ perl -l -p -e 's/__attribute__\(\(__aligned__\(__alignof__\(long long\)\)\)\)//m
 perl -l -p -e 's/__attribute__\(\(__aligned__\(__alignof__\(long double\)\)\)\)//mg' libnx_bindings/libnx_pp5.c > libnx_bindings/libnx_pp6.c
 perl -l -p -e 's/__extension__//mg' libnx_bindings/libnx_pp6.c > libnx_bindings/libnx_pp7.c
 perl -l -p -e 's/_Alignas\(8\)//mg' libnx_bindings/libnx_pp7.c > libnx_bindings/libnx_pp8.c
-perl -l -p -e 's/^typedef unsigned int wchar_t;\z$n//mg' libnx_bindings/libnx_pp8.c > libnx_bindings/libnx_pp9.c
-perl -l -p -e 's/__inline/inline/mg' libnx_bindings/libnx_pp9.c > libnx_bindings/libnx_pp10.c
-perl -l -p -e 's/^extern struct in_addr __nxlink_host;\z$n//mg' libnx_bindings/libnx_pp10.c > libnx_bindings/libnx_pp11.c
-perl -0 -l -p -e 's/ static inline uint32_t $n__crc32.*$n\{$n(.*)$n\}$n//smg' libnx_bindings/libnx_pp11.c > libnx_bindings/libnx_pp12.c
-perl -0 -l -p -e 's/static inline u32 crc32.* \{$n(.*)$n\}$n//smg' libnx_bindings/libnx_pp12.c > libnx_bindings/libnx_pp13.c
-perl -0 -l -p -e 's/int pselect \(.*\)$n *;$n//smg' libnx_bindings/libnx_pp13.c > libnx_bindings/libnx_pp14.c
+perl -l -p -e 's/_Bool/bool/mg' libnx_bindings/libnx_pp8.c > libnx_bindings/libnx_pp9.c
+perl -l -p -e 's/^typedef unsigned int wchar_t;\z$n//mg' libnx_bindings/libnx_pp9.c > libnx_bindings/libnx_pp10.c
+perl -l -p -e 's/__inline/inline/mg' libnx_bindings/libnx_pp10.c > libnx_bindings/libnx_pp11.c
+perl -l -p -e 's/^extern struct in_addr __nxlink_host;\z$n//mg' libnx_bindings/libnx_pp11.c > libnx_bindings/libnx_pp12.c
+perl -0 -l -p -e 's/ static inline uint32_t $n__crc32.*$n\{$n(.*)$n\}$n//smg' libnx_bindings/libnx_pp12.c > libnx_bindings/libnx_pp13.c
+perl -0 -l -p -e 's/static inline u32 crc32.* \{$n(.*)$n\}$n//smg' libnx_bindings/libnx_pp13.c > libnx_bindings/libnx_pp14.c
+perl -0 -l -p -e 's/int pselect \(.*\)$n *;$n//smg' libnx_bindings/libnx_pp14.c > libnx_bindings/libnx_pp15.c
 
 printf '%%module libnx\n%%{\n#include <switch.h>\n%%}\n' > libnx_bindings/libnx_header_swig.c
 
-cat libnx_bindings/libnx_header_swig.c libnx_bindings/libnx_pp14.c > libnx_bindings/libnx.i
+cat libnx_bindings/libnx_header_swig.c libnx_bindings/libnx_pp15.c > libnx_bindings/libnx.i
 
 swig -python libnx_bindings/libnx.i
 
