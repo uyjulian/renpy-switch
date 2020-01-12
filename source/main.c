44eeb7a65ef875efdf5e1106ef851b62d789c077
uyjulian <uyjulian@gmail.com>
2020-01-11 23:45:56 -0600

    Major refractoring of launcher

--- source/main.c
+++ source/main.c
@@ -83,6 +83,12 @@ void userAppExit()
 void __nx_win_init() {}
 void __nx_win_exit() {}
 
+char relative_dir_path[0x400];
+char sysconfigdata_file_path[0x400];
+char python_home_buffer[0x400];
+char python_snprintf_buffer[0x400];
+char python_script_buffer[0x400];
+
 int main(int argc, char* argv[])
 {
     setenv("MESA_NO_ERROR", "1", 1);
@@ -94,6 +100,7 @@ int main(int argc, char* argv[])
     Py_NoSiteFlag = 1;
     Py_IgnoreEnvironmentFlag = 1;
     Py_NoUserSiteDirectory = 1;
+    Py_DontWriteBytecodeFlag = 1;
     Py_OptimizeFlag = 2;
 
     static struct _inittab builtins[] = {
@@ -161,89 +168,142 @@ int main(int argc, char* argv[])
         {NULL, NULL}
     };
 
-    FILE* sysconfigdata_file = fopen("romfs:/Contents/lib/python2.7/_sysconfigdata.py", "rb");
-    if (sysconfigdata_file == NULL)
+    if (argc != 1)
     {
-        sysconfigdata_file = fopen("./lib/python2.7/_sysconfigdata.py", "rb");
-        if (sysconfigdata_file == NULL)
+        fprintf(stderr, "Only one argument (the program itself) should be passed to the program.\n");
+        Py_Exit(1);
+    }
+
+    if (strchr(argv[0], ' '))
+    {
+        fprintf(stderr, "No spaces should be contained in the program path.\n");
+        Py_Exit(1);
+    }
+
+    if (!strchr(argv[0], ':'))
+    {
+        fprintf(stderr, "Program path does not appear to be an absolute path.\n");
+        Py_Exit(1);
+    }
+
+    char* last_dir_separator = strrchr(argv[0], '/');
+
+    if (last_dir_separator)
+    {
+        size_t dirpath_size = last_dir_separator - argv[0];
+        memcpy(relative_dir_path, argv[0], dirpath_size);
+        relative_dir_path[dirpath_size] = '\000';
+    }
+
+    char* dir_paths[] = {
+        "romfs:/Contents/",
+        relative_dir_path,
+        NULL,
+    };
+
+    int found_sysconfigdata = 0;
+    int found_renpy = 0;
+
+    for (int i = 0; i < sizeof(dir_paths); i += 1)
+    {
+        if (dir_paths[i] == NULL)
         {
-            fprintf(stderr, "Could not find lib/python2.7/_sysconfigdata.py.\n");
-            Py_Exit(1);
+            break;
         }
-        else
+        snprintf(sysconfigdata_file_path, sizeof(sysconfigdata_file_path), "%slib/python2.7/_sysconfigdata.py", dir_paths[i]);
+        FILE* sysconfigdata_file = fopen((const char*)sysconfigdata_file_path, "rb");
+        if (sysconfigdata_file != NULL)
         {
-            Py_SetPythonHome("./lib/python2.7");
+            found_sysconfigdata = 1;
             fclose(sysconfigdata_file);
         }
+
+        snprintf(python_script_buffer, sizeof(python_script_buffer), "%srenpy.py", dir_paths[i]);
+        FILE* renpy_file = fopen((const char*)python_script_buffer, "rb");
+        if (renpy_file != NULL)
+        {
+            found_renpy = 1;
+            fclose(renpy_file);
+        }
+
+        if (found_sysconfigdata == 1 && found_renpy == 1)
+        {
+            snprintf(python_home_buffer, sizeof(python_home_buffer), "%slib/python2.7", dir_paths[i]);
+            snprintf(python_snprintf_buffer, sizeof(python_snprintf_buffer), "import sys\nsys.path = ['%slib/python2.7']", dir_paths[i]);
+            Py_SetPythonHome(python_home_buffer);
+            break;
+        }
     }
-    else
+
+    if (found_sysconfigdata == 0)
     {
-        Py_DontWriteBytecodeFlag = 1;
-        Py_SetPythonHome("romfs:/Contents/lib/python2.7");
-        fclose(sysconfigdata_file);
+        fprintf(stderr, "Could not find lib/python2.7/_sysconfigdata.py.\n");
+        Py_Exit(1);
+    }
+
+    if (found_renpy == 0)
+    {
+        fprintf(stderr, "Could not find renpy.py.\n");
+        Py_Exit(1);
     }
 
     Py_InitializeEx(0);
 
     PyImport_ExtendInittab(builtins);
 
-    PySys_SetArgv(argc, argv);
+    char* pyargs[] = {
+        python_script_buffer,
+        NULL,
+    };
+
+    PySys_SetArgvEx(1, pyargs, 1);
 
     int python_result;
 
-    python_result = PyRun_SimpleString(
-        "import sys\n"
-
-        "del sys.path[:]\n"
-
-        "pathdirs = ['romfs:/Contents/lib/python2.7', 'romfs:/Contents', './lib/python2.7', './lib']\n"
-
-        "for i in pathdirs:\n"
-        "\tsys.path.append(i)\n"
-
-        "import os\n"
-        "import pygame_sdl2\n"
-
-        "del sys.path[:]\n"
-
-        "for i in pathdirs:\n"
-        "\tj = os.path.abspath(os.path.realpath(i))\n"
-        "\tif os.path.exists(j):\n"
-        "\t\tsys.path.append(j)\n"
-
-        "import os\n"
-        "import pygame_sdl2\n"
-        "import encodings\n"
-    );
+    python_result = PyRun_SimpleString(python_snprintf_buffer);
 
     if (python_result == -1)
     {
-        fprintf(stderr, "Could not import the python libraries.\n");
+        fprintf(stderr, "Could not set the Python path.\n");
         Py_Exit(1);
     }
 
-    FILE* renpy_file = fopen("romfs:/Contents/renpy.py", "rb");
-    if (renpy_file == NULL)
+    char* py_libs[] = {
+        "os",
+        "pygame_sdl2",
+        "encodings",
+        NULL,
+    };
+
+    for (int i = 0; i < sizeof(py_libs); i += 1)
     {
-        renpy_file = fopen("./renpy.py", "rb");
-        if (renpy_file == NULL)
+        if (py_libs[i] == NULL)
         {
-            fprintf(stderr, "The renpy.py file is not located in the same directory as this executable.\n");
+            break;
+        }
+        snprintf(python_snprintf_buffer, sizeof(python_snprintf_buffer), "import %s", py_libs[i]);
+        python_result = PyRun_SimpleString(python_snprintf_buffer);
+        if (python_result == -1)
+        {
+            fprintf(stderr, "Could not import python library %s.\n", py_libs[i]);
             Py_Exit(1);
         }
-        else
-        {
-            python_result = PyRun_SimpleFileEx(renpy_file, "./renpy.py", 1);
-        }
+    }
+
+    FILE* renpy_file = fopen((const char*)python_script_buffer, "rb");
+    if (renpy_file == NULL)
+    {
+        fprintf(stderr, "Could not open renpy.py after Python initialization.\n");
+        Py_Exit(1);
     }
     else
     {
-        python_result = PyRun_SimpleFileEx(renpy_file, "romfs:/Contents/renpy.py", 1);
+        python_result = PyRun_SimpleFileEx(renpy_file, (const char*)python_script_buffer, 1);
     }
 
     if (python_result == -1)
     {
-        fprintf(stderr, "An uncaught Python exception occurred.\n");
+        fprintf(stderr, "An uncaught Python exception occurred during renpy.py execution.\n");
         Py_Exit(1);
     }
 
